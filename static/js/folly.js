63984a773357b146530d22c991c292d0d2f6cfc8
Chris Boyle <chris@boyle.name>
2013-10-15 22:39:02 +0100

    https favicons (fix mixed content warning)

--- static/js/folly.js
+++ static/js/folly.js
@@ -1,4 +1,4 @@
 $(function(){
 $("a.addfavicon").each(function() {
-    $(this).before('<img class="favicon" src="http://g.etfv.co/http://' + this.hostname + '/" />');
+    $(this).before('<img class="favicon" src="https://getfavicon.appspot.com/http://' + this.hostname + '/" />');
 });})
