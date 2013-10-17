a85a0bc95a0d2aec707ea36efc1c7593bae654a4
Chris Boyle <chris@boyle.name>
2013-10-17 20:46:53 +0100

    Allow links on root page, for OpenID

--- settings.py
+++ settings.py
@@ -272,6 +272,18 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "folly_context.context_processors.site",
 )
 
+TEMPLATE_ACCESSIBLE_SETTINGS=(
+        "ACCOUNTS_VERIFICATION_REQUIRED", "BITLY_ACCESS_TOKEN",
+        "BLOG_USE_FEATURED_IMAGE", "COMMENTS_DISQUS_SHORTNAME",
+        "COMMENTS_NUM_LATEST", "COMMENTS_DISQUS_API_PUBLIC_KEY",
+        "COMMENTS_DISQUS_API_SECRET_KEY", "COMMENTS_USE_RATINGS",
+        "DEV_SERVER", "FORMS_USE_HTML5", "GRAPPELLI_INSTALLED",
+        "GOOGLE_ANALYTICS_ID", "JQUERY_FILENAME", "LOGIN_URL", "LOGOUT_URL",
+        "SITE_TITLE", "SITE_TAGLINE",
+        "ROOT_LINKS",
+    )
+
+
 # List of middleware classes to use. Order is important; in the request phase,
 # these middleware classes will be applied in the order given, and in the
 # response phase the middleware will be applied in reverse order.
